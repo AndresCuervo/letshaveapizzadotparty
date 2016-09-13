611c09c2e93edfa76e497aeb15686c51a1a6517a
Andres Cuervo <andrescuervor@gmail.com>
2016-09-13 15:54:33 -0400

    Automated commit at 2016-09-13 19:54:33 UTC by middleman-deploy 1.0.0

---
+++ javascripts/all-68b579ca.js
@@ -0,0 +1,14 @@
+var fullWidth = window.innerWidth;
+var fullHeight = window.innerHeight;
+var text = '🎉    🍕';
+var elem = document.createElement("div");
+
+// window.onload = function() {
+//     while (true) {
+//         elem.textContent = text;
+//         elem.style.position = "absolute";
+//         elem.style.left = Math.round(Math.random() * fullWidth) + "px";
+//         elem.style.top = Math.round(Math.random() * fullHeight) + "px";
+//         document.body.appendChild(elem);
+//     }
+// };
